afecfe63eaa8878ad3036189f93750ebb013f0c6
DINNERman <shrwnwilliams@gmail.com>
2021-03-23 16:07:11 -0400

    added most if not all dom variables to script

---
+++ assests/js/script.js
@@ -0,0 +1,24 @@
+// dom variables
+var questionArea = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
+var answerArea = document.getElementById("options");
+var timerArea = document.getElementById("timer")
+var answerButtons = document.querySelectorAll(".option");
+var answerButtonOne = document.getElementById("option-one");
+var answerButtonTwo = document.getElementById("option-two");
+var answerButtonThree = document.getElementById("option-three");
+var answerButtonFour = document.getElementById("option-four");
+
+// javascript variables
+
+// function definitions
+
+// event listeners
+
+// functions calls
+
+// answerButtonOne.addEventListener("click", function() {
+//     count++;
+//     setCounterText();
+//   }
+// );
